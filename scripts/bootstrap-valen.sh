4950aa68b055d1464e5c1c15a2d72b8bab44a4f7
Davin Thompson <dthompso99@gmail.com>
2026-08-06 09:50:58 -0400

    Harden native bootstrap helper

--- scripts/bootstrap-valen.sh
+++ scripts/bootstrap-valen.sh
@@ -1,8 +1,16 @@
-echo "Bootstrapping valen compiler..."
-node bootstrap/compiler.js  src/valen.ar valen0
+#!/bin/sh
+
+set -eu
+
+cleanup() {
+    rm -f valen0 valen1 valen0.o valen0.s valen1.o valen1.s valen.o valen.s
+}
+trap cleanup EXIT
+
+echo "Bootstrapping Valen compiler..."
+node bootstrap/compiler.js src/valen.ar valen0
 echo "Compiling Generation 1 valen compiler..."
 ./valen0 src/valen.ar -o valen1
 echo "Compiling final valen compiler..."
 ./valen1 src/valen.ar -o valen
 echo "Cleaning up temporary files..."
-rm valen0 valen1 valen0.o valen0.s valen1.o valen.o
